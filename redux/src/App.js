4e84569e4bd8b086f69f246cdcaf1de7d8225cca
unknown <yugmeshk2139@gmail.com>
2022-04-24 10:02:09 +0530

    redux

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index";
+// function App() {
+//   const counter = useSelector((state) => state.counter);
+//   const dispatch = useDispatch();
+//   const increment = () => {
+//     dispatch({ type: "INC" });
+//   };
+//   const decrement = () => {
+//     dispatch({ type: "DEC" });
+//   };
+//   const addBy = () => {
+//     dispatch({ type: "ADD", payload: 10 });
+//   };
+//   return (
+//     <div className="App">
+//       <h1>Counter App</h1>
+//       <h2>{counter}</h2>
+//       <button onClick={increment}>increment</button>
+//       <button onClick={decrement}>decrement</button>
+//       <button onClick={addBy}>Add By 10</button>
+//     </div>
+//   );
+// }
 
+// export default App;
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment());
+  };
+  const decrement = () => {
+    dispatch(actions.decrement());
+  };
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={addBy}>Add By 10</button>
     </div>
   );
 }
